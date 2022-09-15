4003492af8c5b80ed06f324a4b7caef390a3f5cb
Serhii <serhiibulynin007@gmail.com>
2022-09-15 16:35:17 +0300

    add login markup

--- src/Screens/LoginScreen/LoginScreen.jsx
+++ src/Screens/LoginScreen/LoginScreen.jsx
@@ -1,8 +1,19 @@
-import { View } from "native-base";
+import { Input, KeyboardAvoidingView, View } from "native-base";
 import React from "react";
+import { Keyboard, Platform, TouchableWithoutFeedback } from "react-native";
 
 const LoginScreen = () => {
-  return <View></View>;
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <Input value={value}></Input>
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };
 
 export default LoginScreen;
